05447705595bedb5ecef181b5b62180fc7a9b0cc
Okan Arik <okan.arik@icloud.com>
2022-04-16 00:51:46 +0300

    Düzenlenme yapıldı

--- WebApi/DBOperations/MovieStoreDbContext.cs
+++ WebApi/DBOperations/MovieStoreDbContext.cs
@@ -11,9 +11,8 @@ namespace WebApi.DBOperations
         public DbSet<Actor> Actors {get; set;}
         public DbSet<Genre> Genres {get; set;}
         public DbSet<Director> Directors {get; set;}
-
         public DbSet<MovieActor> MovieActors { get; set; }
-        public DbSet<Movie> DirectorMovies { get; set; }
+
 
 
          protected override void OnModelCreating(ModelBuilder modelBuilder)
